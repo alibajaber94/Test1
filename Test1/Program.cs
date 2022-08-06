48034983f13f3d32029387e94f4c242b38c629be
ali_b <ali_b@DESKTOP-1MUPSKA>
2022-08-06 14:25:23 +0300

    Add project files.

---
+++ Test1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double vatRate = 1.15;
+            float discountRate, productPrice;
+
+            Console.WriteLine("Enter the product price: ");
+            productPrice = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the discount rate: ");
+            discountRate = float.Parse(Console.ReadLine());
+
+        }
+    }
+}
